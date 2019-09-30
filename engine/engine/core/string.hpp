f187c00351a3fcaec703c1a5b0b7f60d05e0e622
Tanki Zhang <tanki_jong@hotmail.com>
2019-09-30 01:17:39 -0400

    remove mis committed change

--- engine/engine/core/string.hpp
+++ engine/engine/core/string.hpp
@@ -9,8 +9,3 @@ std::string Stringf( const char* format, ... );
 std::string Stringf( const int maxLength, const char* format, ... );
 
 std::wstring ToWString(const std::string_view str);
-
-namespace str
-{
-   using namespace fmt;
-}
